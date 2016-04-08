82e413bcde6ac7cc95c3f4b3d98a9621c04a13ef
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-08 05:51:42 +0200

    curly 4 - rank:253 (2015.05.12)

--- Multi/Tron.java
+++ Multi/Tron.java
@@ -5,14 +5,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Player {
+    // Store next move with number of valid steps possible from that move
+    static class Step {
+        Step(String aMove, int aCount) { move = aMove; count = aCount; } 
+        String  getMove()  { return move; }
+        int     getCount() { return count; }
+        @Override
+        public String toString() { return move + " (" + count + ")"; }
+        private String move;                                                // next move
+        private int count;                                                  // number of valid steps from next move
+    } // class Step()
 
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);       
+        Scanner in = new Scanner(System.in);
         // Initializing values
         String move = "LEFT";                                               // Starting direction
         String[] moves = new String[3];                                     // Order of next moves according to strategy
@@ -22,10 +28,10 @@ class Player {
         // game loop
         while (true) {
             int N = in.nextInt();                                           // total number of players (2 to 4).
-            int P = in.nextInt();                                           // your player number (0 to 3).   
+            int P = in.nextInt();                                           // your player number (0 to 3).
             if (firstRound) {                                               // Initializing array of sets
                 for (int i = 0; i < N; ++i) {
-                    reserved.add(new HashSet<Point>()); 
+                    reserved.add(new HashSet<Point>());
                 }
             }
             for (int i = 0; i < N; i++) {
@@ -35,63 +41,47 @@ class Player {
                 int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
                 Point p1 = new Point(X1, Y1);
                 if (firstRound) {
-                    Point p0 = new Point(X0,Y0);                   
+                    Point p0 = new Point(X0,Y0);
                     reserved.get(i).add(p0);
                     System.err.println((i == P ? "Me    " : "Player") + i + ":" + coords(p0) + " added (p0)");
                 }
                 if (i == P) {
                     reserved.get(i).add(p1);
-                    System.err.println("Me    " + i + ":" + coords(p1) + " added");                    
+                    System.err.println("Me    " + i + ":" + coords(p1) + " added");
                     actP.move(X1, Y1);
                 } else {
                     if (p1.getX() != -1) {
                         reserved.get(i).add(p1);
-                        System.err.println("Player" + i + ":" + coords(p1) + " added");   
+                        System.err.println("Player" + i + ":" + coords(p1) + " added");
                     } else {
                         if (reserved.get(i) != null) {
                             System.err.println("Player" + i + " died. Removing its reserved locations.");
-                            reserved.set(i, null);                        
+                            reserved.set(i, null);
                         }
                     }
                 }
             }
             if (firstRound) { firstRound = false; }                         // Store (X0, Y0) coords only first time
-            // Determine possible next positions to step excluding bounds           
-            moves = curlyMoves(move);                                       // Set strategy
-            //String nextMove = null;         
-            Vector<String> optMove = new Vector<>();                        // Store move options
-            Vector<String> safeMove = new Vector<>();                       // Store moves considered safe (not go to pipe)
-            for (int i = 0; i < moves.length; ++i) {               
-                if (!hitWall(moves[i], actP) && !isReserved(getNextP(moves[i], actP), reserved, N)) {
-                    optMove.add(moves[i]);
-                }
-            }
-            if (optMove.isEmpty()) {
+            // Determine possible next positions to step excluding bounds
+            moves = getCurlyMoves(move);                                    // Set strategy
+            //Vector<String> safeMoves = new Vector<>();                    // Store moves considered safe (not a trap)
+            Vector<String> validMoves = getValidMoves(moves, actP, reserved, N);    // Store valid moves
+            if (validMoves.isEmpty()) {
                 System.err.println("OH, fuck! We lost. Waiting for timeout...");
-            } else {
-                for (String m : optMove) {
-                    System.err.println("opt.move: " + m);
-                    if (!isTrap(m, reserved, actP, N)) {
-                        safeMove.add(m);
-                        System.err.println("move " + m + " considered safe");
-                    }
-                }
-                if (safeMove.isEmpty()) {
-                    move = optMove.firstElement();                          // Choose first from options                
-                } else {
-                    move = safeMove.firstElement();                         // Choose first safe move
-                }
-                System.out.println(move);                                   // A single line with UP, DOWN, LEFT or RIGHT                
-            }
+            } else {                                                        // We have at least 1 valid move
+				Vector<String> safeMoves = getSafeMoves(validMoves, actP, reserved, N);
+				move = safeMoves.firstElement();
+				System.out.println(move);
+			}
         } // while()
     } // main()
-    
+
     // Return coordinates of p as string - used for debugging
     private static String coords(Point p) {
-        return "[x=" + (int)p.getX() + ",y=" + (int)p.getY() + "]";  
+        return "[x=" + (int)p.getX() + ",y=" + (int)p.getY() + "]";
     }
     // Our strategy: csiga
-    private static String[] curlyMoves(String move) {
+    private static String[] getCurlyMoves(String move) {
         String[] moves = null;
         switch (move) {
             case "LEFT" : moves = new String[] {"UP", "LEFT", "DOWN"}; break;
@@ -106,31 +96,29 @@ class Player {
         boolean allocated = false;
         for (int i = 0; i < N && !allocated; ++i) {
             if (reserved.get(i) != null) {
-                if (reserved.get(i).contains(p)) {
-                    allocated = true;
-                }    
+                if (reserved.get(i).contains(p)) { allocated = true; }
             }
         }
         return allocated;
     }
     // Returns true if next move is within bounds, false otherwise
-    private static boolean hitWall(String nextMove, Point actP) {
+    private static boolean hitsWall(String move, Point actP) {
         int X = (int)(actP.getX());
         int Y = (int)(actP.getY());
         boolean ret = false;
-        switch(nextMove) {
-            case "LEFT" : if (X == 0) ret=true; break;
-            case "RIGHT": if (X == 29) ret=true; break;
-            case "UP"   : if (Y == 0) ret=true; break;
-            case "DOWN" : if (Y == 19) ret=true; break;
+        switch(move) {
+            case "LEFT" : if (X == 0)   ret=true; break;
+            case "RIGHT": if (X == 29)  ret=true; break;
+            case "UP"   : if (Y == 0)   ret=true; break;
+            case "DOWN" : if (Y == 19)  ret=true; break;
         }
         return ret;
     }
     // Returns position of next move as a point
-    private static Point getNextP(String nextMove, Point actP) {
+    private static Point getP(String move, Point actP) {
         int X = (int)(actP.getX());
         int Y = (int)(actP.getY());
-        switch(nextMove) {
+        switch(move) {
             case "LEFT" : return new Point(X - 1, Y);
             case "RIGHT": return new Point(X + 1, Y);
             case "UP"   : return new Point(X, Y - 1);
@@ -138,8 +126,66 @@ class Player {
         }
         return null;
     }
-    // Check if next move would be considered as a trap
-    private static boolean isTrap(String nextMove, List<HashSet<Point>> reserved, Point actP, int N) {
-        return false;
+    // Returns valid moves (if there are any) from moves, otherwise returns an empty vector
+    private static Vector<String> getValidMoves (String[] moves, Point actP, List<HashSet<Point>> reserved, int N) {
+        Vector<String> validMoves = new Vector<>();
+        for (String move : moves) {
+            if (!hitsWall(move, actP) && !isReserved(getP(move, actP), reserved, N)) {
+                validMoves.add(move);
+            }
+        }
+        return validMoves;
+    }
+
+    // Returns moves ordered by safetiness (first is the most safe)
+    private static Vector<String> getSafeMoves(Vector<String> moves, Point actP, List<HashSet<Point>> reserved, int N) {
+        Vector<Player.Step> steps = new Vector<>();     // Store next moves with valid steps possible from those moves
+        Vector<String> safeMoves = new Vector<>();
+		//Vector<Integer> count = new Vector<>();
+        for (String move : moves) {
+            String[] nextMoves = getCurlyMoves(move);
+			int validCount = 0;
+			Point p = getP(move, actP);
+			for (String m : nextMoves) {
+				if (!hitsWall(m, p) && !isReserved(getP(m, p), reserved, N)) {
+					validCount++;
+				}
+			}
+			//count.add(validCount);
+			steps.add(new Step(move, validCount));
+        }      
+		for(Step step : steps) {                        // Put moves with 3 or 2 valid directions to safeMoves
+			if (step.getCount() >= 2) {
+				safeMoves.add(step.getMove());
+			}
+		}
+		for(Step step : steps) {                        // Put moves with 1 valid direction to safeMoves
+			if (step.getCount() == 1) {
+				safeMoves.add(step.getMove());
+			}
+		}
+		for(Step step : steps) {                        // Put moves with 0 valid directions to safeMoves
+			if (step.getCount() == 0) {
+				safeMoves.add(step.getMove());
+			}
+		}
+		System.err.println("Steps:");
+		for (Step step : steps) {
+		    System.err.println(step);
+		}
+		if (steps.size() == 2) {
+		    int count1 = steps.get(0).getCount();
+		    int count2 = steps.get(1).getCount();
+		    if ((count1 == 1 && count2 == 3) || (count2 == 1 && count1 == 3)) {
+    		    System.err.println("Corner detected - entering corner...");
+    		    safeMoves.clear();
+    		    safeMoves.add(count1 == 1 ? steps.get(0).getMove() : steps.get(1).getMove());		        
+		    }
+		}
+		System.err.println("Safe moves:");
+		for (int i = 0; i < safeMoves.size(); ++i) {
+		    System.err.println(safeMoves.get(i));
+		}
+        return safeMoves;
     }
 }

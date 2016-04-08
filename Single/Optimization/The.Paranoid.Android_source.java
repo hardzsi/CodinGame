530b3d48c2d04775467f771e608e5f9a7af7eaf1
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-08 07:07:31 +0200

    v3b chars:581 (2015.05.29)

--- Single/Optimization/The.Paranoid.Android_source.java
+++ Single/Optimization/The.Paranoid.Android_source.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Player{
 	public static void main(String[] A){
 		Scanner s=new Scanner(System.in);
-		int F=s.nextInt();s.next();s.next();s.next();int X=s.nextInt();s.next();s.next();int E=s.nextInt();
-		Integer[][] d=new Integer[E][2];
-		for(int i=0;i<E;i++){
+		int F=s.nextInt(),X,E,L,P,T,i;s.next();s.next();s.next();X=s.nextInt();s.next();s.next();
+		Integer[][] d=new Integer[E=s.nextInt()][2];
+		for(i=0;i<E;i++){
 			d[i][0]=s.nextInt();d[i][1]=s.nextInt();
 		}
 		Arrays.sort(d,new Comparator<Integer[]>(){
@@ -13,11 +13,11 @@ class Player{
 				return a[0].compareTo(b[0]);
 			}
 		});
-		while(F>0){
-			int L=s.nextInt(),P=s.nextInt();
+		for(;;){
+			L=s.nextInt();P=s.nextInt();
 			String g=s.next(),m="WAIT";
-			if(L!=-1){
-				int T=L<F-1?d[L][1]:X;
+			if(L>-1){
+				T=L<F-1?d[L][1]:X;
 				m=T<P&g.equals("RIGHT")|T>P&g.equals("LEFT")?"BLOCK":m;
 			}
 			System.out.println(m);

d8fd34dbda1b9d46a64e407f4540f92ae8dd1844
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-08 06:45:59 +0200

    chars:1381 (2015.05.19)

--- Single/Optimization/The.Paranoid.Android.java
+++ Single/Optimization/The.Paranoid.Android.java
@@ -1,43 +1,38 @@
-// The Paranoid Android opt3 - 2000
 import java.util.*;
 class Player {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int levels = in.nextInt();  // floors num
-        int width = in.nextInt();   // area width
-        in.nextInt(); in.nextInt(); // max num of rounds | floor where exit is
-        int exitPos = in.nextInt(); // exit pos
-        in.nextInt(); in.nextInt(); // clones num | other lifts
-        int lifts = in.nextInt();   // lifts num
-        boolean[] blocked = new boolean[levels]; // floor blocked yet or no
-        Integer[][] data = new Integer[lifts][2];// sortable lift data
+        int levels = in.nextInt();
+        in.nextInt();
+        in.next(); in.next();
+        int exitPos = in.nextInt();
+        in.next(); in.next();
+        int lifts = in.nextInt();
+        boolean[] blocked = new boolean[levels];
+        Integer[][] data = new Integer[lifts][2];
         for (int i = 0; i < lifts; i++) {
-            data[i][0] = in.nextInt(); // floor where lift is
-            data[i][1] = in.nextInt(); // lift pos on its floor
+            data[i][0] = in.nextInt();
+            data[i][1] = in.nextInt();
         }
-        Arrays.sort(data, new Comparator<Integer[]>() { // sort data by 1st dim
-            @Override
-            public int compare(final Integer[] arg1, final Integer[] arg2) {
-                final Integer a = arg1[0];
-                final Integer b = arg2[0];
-                return a.compareTo(b);
+        Arrays.sort(data, new Comparator<Integer[]>() {
+            public int compare(Integer[] arg1, Integer[] arg2) {
+                return arg1[0].compareTo(arg2[0]);
             }
         });
-        // game loop
         while (true) {
-            int cloneLev = in.nextInt();// floor of lead clone, -1 if dead
-            int clonePos = in.nextInt();// lead clone pos, -1 if dead
-            String dir = in.next();     // lead clone dir: LEFT,RIGHT or NONE if dead
-            String move = "WAIT";
-            if (cloneLev != -1) {       // If no clone arrives, wait next round
+            int cloneLev = in.nextInt(),
+                clonePos = in.nextInt();
+            String dir = in.next(),
+                   move = "WAIT";
+            if (cloneLev != -1) {
                 int targetPos = (cloneLev < levels - 1) ? data[cloneLev][1] : exitPos;
-                if ((targetPos < clonePos && dir.equals("RIGHT") && !blocked[cloneLev]) ||
-                    (targetPos > clonePos && dir.equals("LEFT")  && !blocked[cloneLev])) {
+                if ((targetPos < clonePos & dir.equals("RIGHT") & !blocked[cloneLev]) |
+                    (targetPos > clonePos & dir.equals("LEFT")  & !blocked[cloneLev])) {
                     move = "BLOCK";
                     blocked[cloneLev] = true;
                 }
             }
-            System.out.println(move); // WAIT or BLOCK
+            System.out.println(move);
         }
     }
 }

75e66da37c74aa970a8b5b34dea5d0a5fc9fa1c6
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-09 05:50:48 +0200

    Initial - chars:307 (2015.05.20)

---
+++ Single/Optimization/Power.of.Thor.java
@@ -0,0 +1,1 @@
+import java.util.*;class Player{public static void main(String[] a){Scanner s=new Scanner(System.in);int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt();while(true){s.next();String m="";if(y<Y){m="N";Y--;}else if(y>Y){m="S";Y++;}if(x>X){m+="E";X++;}else if(x<X){m+="W";X--;}System.out.println(m);}}}

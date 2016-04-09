c179d7c6f01d65945a489fd83f50e7289d8687f5
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-09 17:44:18 +0200

    pass:60% - simplify markNeighbors(), other small changes (2015.06.13)

--- Single/Medium/Teads.Sponsored.Challenge.java
+++ Single/Medium/Teads.Sponsored.Challenge.java
@@ -18,7 +18,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int xi = in.nextInt();                          // ID of a person which is adjacent to yi
             int yi = in.nextInt();                          // ID of a person which is adjacent to xi
-            debug(xi + " - " + yi);
+            //debug(xi + " - " + yi);
             ids.add(xi); ids.add(yi);
             Node<Integer> upper = getNode(xi, nodes);       // Get upper node from nodes array
             if (upper == null) {                            // Create and store new upper node if not found among nodes
@@ -34,20 +34,26 @@ class Solution {
             } else {                                        // Found lower node among nodes
                 lower.addParent(upper);                     // Add upper node as parent to lower node
                 if (lower.getLevel() != (upper.getLevel() + 1)) {
-                    // Set lower's level to its parent+1 and
-                    // re-level all its children accordingly
-                    reLevelNode(lower, upper, nodes, upper.getLevel() + 1); 
+                    reLevelNode(lower, upper, nodes, upper.getLevel() + 1); // Re-level node's all children and other parents
                 }
             }
-            //lower.addParent(upper);                         // Add upper node as parent to lower node
             upper.addChild(lower);                          // Add lower node as child to upper node
         }
         digLevel = Math.round(DIG_PERCENT * numLevels(nodes));
         //digLevel = n < 500 ? numLevels(nodes) : Math.round(DIG_PERCENT * numLevels(nodes));      
+        // Count nodes
+        /*int[] count = new int[10];
+        for (Node<Integer> node : nodes) {
+            int p = node.getParents().size();
+            count[p]++;
+        }
+        for (int i = 0; i < count.length; ++i) {
+            disp(i + " parents: " + count[i]);
+        }*/
         // Display nodes
         /*debug("\n" + nodes.size() + " nodes:");
         for (Node<Integer> node : nodes) {
-            debug(node.toString());
+            disp(node.toString());
         }*/
         disp("\n" + numLevels(nodes) + " levels, digLevel:" + digLevel + ", " + nodes.size() + " nodes in nodes array");
 
@@ -70,8 +76,8 @@ class Solution {
             }
         }
         Collections.sort(steps);                            // Sorting steps to get the minimum step amount (first one)
-        /*// Display steps
-        debug("\ndetermined steps:");
+        // Display steps
+        /*debug("\ndetermined steps:");
         for (Integer stp : steps) {
             debug(stp.toString());
         }*/
@@ -80,36 +86,38 @@ class Solution {
     }// main()
 
     // Re-level a node to the level given as argument, as well as all its children recursively
-    static <T> void reLevelNode(Node<T> node, Node<T> upper, List<Node<T>> nodes, int level) {
-        disp("re-levelling " + node + "  to level " + level + " , upper is:" + upper.getId());
+    static <T> void reLevelNode(Node<T> node, Node<T> blocked, List<Node<T>> nodes, int level) {
+        //disp("re-levelling " + node + "  to level " + level + " , upper is:" + upper.getId());
         node.setLevel(level);
-        /*if (node.hasParents()) {
+        if (node.hasParents()) {
             Set<Node<T>> parents = node.getParents();
+            debug("node " + node.getId() + " has " + parents.size() + " parents");
             for (Node<T> parent : parents) {
-                reLevelNode(getNode(parent.getId(), nodes), upper, nodes, node.getLevel() - 1);
+                if (!parent.equals(blocked)) {
+                    debug("parent is:" + parent.getId() + ", blocked is:" + blocked.getId());
+                    //reLevelNode(getNode(parent.getId(), nodes), blocked, nodes, node.getLevel() - 1);                
+                }
             }
-        }*/
+        }
         if (node.hasChildren()) {
             Set<Node<T>> children = node.getChildren();
             for (Node<T> child : children) {
-                reLevelNode(getNode(child.getId(), nodes), upper, nodes, node.getLevel() + 1);
+                //disp(" -> ");
+                reLevelNode(getNode(child.getId(), nodes), blocked, nodes, node.getLevel() + 1);
             }
         }
     }// reLevelNode()
 
-    // Determine steps needed to propagate the whole ad from given start
-    // node id via collecting marked nodes and also mark their neighbors
+    // Determine steps needed to propagate the whole ad from given start node
     static <T> Integer getSteps(T id, List<Node<T>> nodes) {
         debug("starting getSteps with node " + id);
-        List<Node<T>> markedNodes = new ArrayList<>();
+        List<Node<T>> markedNodes = new ArrayList<>();      // Collect marked nodes per iteration - Set<> would be faster?
         int step = 1;
-        boolean allMarked = true;
+        boolean allMarked;
         do {
             // Collect marked nodes
             for (Node<T> node : nodes) {
-                if (node.isMarked()) {
-                    markedNodes.add(node);
-                }
+                if (node.isMarked()) { markedNodes.add(node); }
             }
             // Mark neighbors of marked nodes (cannot be concatenated with for() cycle above)
             for (Node<T> marked : markedNodes) {
@@ -120,32 +128,21 @@ class Solution {
             // Check if all nodes are marked
             allMarked = true;
             for (Node<T> node : nodes) {
-                if (!node.isMarked()) {
-                    allMarked = false;
-                }
+                if (!node.isMarked()) { allMarked = false; }
             }
             debug("step " + step);
         } while (!allMarked);
         return step;
     }// getSteps()
 
-    // Mark node and all its neighbors (parent and children)
+    // Mark node and all its neighbors (parents and children)
     // Warning: existence of node NOT checked, but come from nodes array so it should exist
     static <T> void markNeighbors(Node<T> node, List<Node<T>> nodes) {
         node.mark();                                        // Mark the node
-        if (node.hasChildren()) {                           // Mark node's children
-            Set<Node<T>> children = node.getChildren();
-            for (Node<T> c : children) {
-                Node<T> child = getNode(c.getId(), nodes);
-                child.mark();
-            }
-        }
-        if (node.hasParents()) {                           // Mark node's parents
-            Set<Node<T>> parents = node.getParents();
-            for (Node<T> p : parents) {
-                Node<T> parent = getNode(p.getId(), nodes);
-                parent.mark();
-            }
+        Set<Node<T>> neighbors = node.getChildren();        // Put all neighbors in a set
+        neighbors.addAll(node.getParents());
+        for (Node<T> neighbor : neighbors) {                // Mark node's parents and children
+            getNode(neighbor.getId(), nodes).mark();
         }
     }// markNeighbors()
 
@@ -159,7 +156,7 @@ class Solution {
         return null;
     }// getNode()
 
-    // Return the number of levels (counting from 0)
+    // Return number of levels (numbering starts from 0)
     static <T> int numLevels(List<Node<T>> nodes) {
         int lowest = 0;
         for (Node<T> node : nodes) {
@@ -169,19 +166,15 @@ class Solution {
     }// numLevels()
 
     // Display string to err either if DEBUG true or always
-    static void debug(String s) {
-        if (DEBUG) { System.err.println(s); }
-    }
+    static void debug(String s) { if (DEBUG) { System.err.println(s); } }
     static void disp(String s) { System.err.println(s); }
 }// class Solution
 
 
-// <<<<<<<<<<<<<<<<< Node class >>>>>>>>>>>>>>>>>>>
-// Generic node class with id and flag to be marked
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Node class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Generic node class with any number of parents and children, also id and marked flag
 class Node<T> {
-    public Node(T id) {                                     // Constructor
-        this.id = id;
-    }
+    public Node(T id) { this.id = id; }                     // Constructor
 
     // parents
     public Set<Node<T>> getParents() { return parents; }

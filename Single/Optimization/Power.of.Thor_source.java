0c16369484f6f303e5a1c60154a220f499dbec75
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-09 06:00:07 +0200

    v4b chars:? (2015.05.21)

--- Single/Optimization/Power.of.Thor_source.java
+++ Single/Optimization/Power.of.Thor_source.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Player{
 	public static void main(String[] r){
 		Scanner s=new Scanner(System.in);
-		int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt();
+		int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt(),a,b;
 		while(true){
-			s.next();int a=Y,b=X;
+			s.next();a=Y;b=X;
 			Y+=Math.signum(y-Y);X+=Math.signum(x-X);
 			String[] c={"S","","N","E","","W"};System.out.println(c[a-Y+1]+c[b-X+4]);
 		}

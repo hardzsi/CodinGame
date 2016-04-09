a10ab28d4669e12140065e4644baddacec166c7f
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-09 06:03:11 +0200

    v6 chars:?

--- Single/Optimization/Power.of.Thor_source.java
+++ Single/Optimization/Power.of.Thor_source.java
@@ -1,13 +1,14 @@
-// Power of Thor - Code size 368
+// Power of Thor - Code size 374
 import java.util.*;
 class Player{
 	public static void main(String[] r){
 		Scanner s=new Scanner(System.in);
 		int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt(),a,b;
-		while(String[] c={"S","","N","E","","W"}){
+		while(true){
+			char[] c={83,13,78,69,13,87};
 			s.next();a=Y;b=X;
 			Y+=Math.signum(y-Y);X+=Math.signum(x-X);
-			System.out.println(c[a-Y+1]+c[b-X+4]);
+			System.out.println(""+c[a-Y+1]+c[b-X+4]);
 		}
 	}
 }

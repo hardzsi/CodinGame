63d9460e9562b33557bdca84ee16322e1d767b96
Hargitai Ferenc <fhargitai@gmail.com>
2016-04-09 17:04:23 +0200

    pass:60% - add reLevelChildren(),reLevelNode(); expand Node class with getLevel(),setLevel()

--- Single/Medium/Teads.Sponsored.Challenge.java
+++ Single/Medium/Teads.Sponsored.Challenge.java
@@ -2,7 +2,7 @@
 import java.util.*;
 
 class Solution {
-    static final boolean DEBUG = false;
+    static final boolean DEBUG = true;
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -15,37 +15,46 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int xi = in.nextInt();                          // ID of a person which is adjacent to yi
             int yi = in.nextInt();                          // ID of a person which is adjacent to xi
-            //debug(xi + " - " + yi);
+            debug(xi + " - " + yi);
             if (!ids.contains(xi)) { ids.add(xi); }
             if (!ids.contains(yi)) { ids.add(yi); }
             Node<Integer> upper = getNode(xi, nodes);       // Get upper node form nodes array
             if (upper == null) {                            // Create and store new upper node if not found among nodes
                 upper = new Node<>(null, xi);
                 nodes.add(upper);
+            } else {
+                //debug("upper" + upper + " found in nodes - its children might be re-levelled");
             }
             Node<Integer> lower = getNode(yi, nodes);       // Get lower node form nodes array
             if (lower == null) {                            // Create and store new lower node if not found among nodes
                 lower = new Node<>(upper, yi);
+                lower.setLevel(upper.getLevel() + 1);
                 nodes.add(lower);
+            } else {                                        // Found lower node among nodes
+                lower.addParent(upper);                     // Add upper node as parent to it (lower has no parent)
+                if (lower.getLevel() != (upper.getLevel() + 1)) {
+                    debug("lowernode " + lower.getId() + " level: " + lower.getLevel() + " -> " + (upper.getLevel() + 1));
+                    lower.setLevel(upper.getLevel() + 1);       // Set lower's level to its parent + 1
+                    debug("lower" + lower + " - its children must be re-levelled");
+                    reLevelChildren(lower, nodes);                    
+                }
             }
             upper.addChild(lower.getId());                  // Add lower node as child to upper node
-            if (lower.getParent() == null) {                // Add upper node as parent to lower node if lower node has no parent
-                lower.addParent(upper);
-            }
         }
         //Collections.sort(ids);
+        /*
         // Display ids
         debug("\nnode ids sorted:");
         for (Integer id : ids) {
             debug(id.toString());
-        }
+        }*/
         // Display nodes
         debug("\nnodes:");
         for (Node<Integer> node : nodes) {
             debug(node.toString());
         }
 
-        debug("\ndetermine steps:");
+        debug("\ndetermining steps for all ids...");
         // Determine the steps needed spreading from id and store these in steps array
         for (Integer id : ids) {
             Node<Integer> current = getNode(id, nodes);
@@ -67,6 +76,22 @@ class Solution {
         disp("\noutput:");
         System.out.println(steps.get(0));                   //  Minimal amount of steps required to propagate the ad
     }// main()
+
+    // Re-levelling all child of current node according its level 
+    static <T> void reLevelChildren(Node<T> node, List<Node<T>> nodes) {
+        List<Node<T>> children = new ArrayList<>();
+        children = node.getChildren();
+        for (Node<T> child : children) {
+            reLevelNode(child, node.getLevel() + 1);
+        }
+    }// reLevelChildren()
+
+    // Re-level a node to given level plus its children recursively
+    static <T> void reLevelNode(Node<T> node, int level) {
+        debug("re-level node " + node.getId() + " to level " + level);
+        node.setLevel(level);
+        
+    }// reLevelNode()
 
     // Determine steps needed to propagate the whole ad from given start
     // node id via collecting marked nodes and also mark their neighbors
@@ -93,23 +118,23 @@ class Solution {
     // Mark node and all its neighbors (parent and children)
     // Warning: existence of node is NOT checked!
     static <T> void markNeighbors(Node<T> node, List<Node<T>> nodes) {
-        debug("markNeighbors called for " + node);
+        //debug("markNeighbors called for " + node);
         node.mark();                                        // Mark the node
-        debug("node " + node.getId() + " marked");
+        //debug("node " + node.getId() + " marked");
         if (node.hasChildren()) {                           // Mark node's children
-            debug("node has children");
+            //debug("node has children");
             List<Node<T>> children = node.getChildren();
             for (Node<T> c : children) {
                 Node<T> child = getNode(c.getId(), nodes);
                 child.mark();
-                debug("child node " + child.getId() + " marked:" + child.isMarked());
+                //debug("child node " + child.getId() + " marked:" + child.isMarked());
             }
         }
         if (node.getParent() != null) {                     // Mark node's parent
             node.getParent().mark();
-            debug("node's parent is " + node.getParent() + " marked:" + node.getParent().isMarked());
+            //debug("node's parent is " + node.getParent() + " marked:" + node.getParent().isMarked());
         } else {
-            debug("node has no parent");
+            //debug("node has no parent");
         }
     }// markNeighbors()
 
@@ -155,6 +180,7 @@ class Node<T> {
         this.id = id;
     }// Constructor
     public T        getId() { return id; }                  // Getters
+    public int      getLevel() { return level; }
     public Node<T>  getParent() { return parent; }
     public List<Node<T>> getChildren() { return children; }
     // Return true if node has at least one child
@@ -183,7 +209,8 @@ class Node<T> {
         }
         return ret;
     }// hasChild()
-    // Add child with id and return child node, if node wasn't among children, otherwise return null
+    // Add child with id and return child node, if
+    // node wasn't among children, otherwise return null
     public Node<T>  addChild(T id) {
         Node<T> child = null;
         if (children == null) {
@@ -199,13 +226,14 @@ class Node<T> {
     public void     addParent(Node<T> p) {
         if (parent == null) { parent = p; }
     }// addParent()
-
+    public void     setLevel(int lev) { level = lev; }
     // Set and remove marked flag
     public void     mark() { marked = true; }
     public void     clearMark() { marked = false; }
     // Return true if marked flag is set
     public boolean  isMarked() { return marked; }
-    // Return string as 'node id (parent:id, children:ids)' or 'node id (children:ids  parent:id)'
+    // Return string as 'node id (parent:id, children:ids)'
+    // or as 'node id (children:ids  parent:id)'
     @Override
     public String   toString() {
         String p = "none";
@@ -220,11 +248,12 @@ class Node<T> {
             }
         }
         //return "node " + id + " (parent:" + p + ", children:" + buf.toString() + ")";
-        return "node " + id + " (children:" + buf.toString() + "  parent:" + p + ")";
+        return "node " + id + " (children:" + buf.toString() + "  parent:" + p + ") | level:" + level;
     }// toString()
 
-    private T id = null;                                    // Node id
-    private boolean marked = false;                         // Marked flag
-    private Node<T> parent = null;                          // Parent node
-    private List<Node<T>> children = null;                  // Children nodes
+    private T id = null;                                    // id
+    private int level = 0;                                  // level (root = 0)
+    private boolean marked = false;                         // marked flag
+    private Node<T> parent = null;                          // parent node
+    private List<Node<T>> children = null;                  // children nodes
 }// class Node<>
